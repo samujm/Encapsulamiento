765731f4254c5239f6d7a61e0eaded1c4d682965
Samantha González <samujm76@gmail.com>
2022-08-10 22:08:31 -0500

    Creando metodos get y set

--- src/dominio/Persona.java
+++ src/dominio/Persona.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package dominio;
 
-/**
- *
- * @author Samantha
- */
 public class Persona {
+    //Se crean los atributos
+    private String nombre;
+    private double sueldo;
+    private boolean eliminado;
     
+    //Creando constructor
+    public Persona(String nombre, double sueldo, boolean eliminado){
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+        this.eliminado = eliminado;
+    }
+    
+    //Definir metodos get y set 
+    public String getNombre(){ //Metodo para recuperar el valor
+        return this.nombre; //regresa 
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public double getSueldo(){
+        return this.sueldo; 
+    }
+    
+    public void setSueldo(double sueldo){
+        this.sueldo = sueldo;
+    }
+    
+    public boolean isEliminado(){ //En boolean se utiliza is en lugar de get
+        return this.eliminado; 
+    }
+    
+    public void setEliminado(boolean eliminado){
+        this.eliminado = eliminado;
+    }
 }
